f947acb555fdf58b1d98b9038d9d26a656520292
Leonardo Pereira <leopereira.dev@gmail.com>
2025-03-17 16:53:15 -0300

    Adicionando a funcionalidade de riscar o item ao dar o check na tarefa e utilizando uma melhor combinação de cores

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', toggleTodoItem);
+    });
+});
+
+function toggleTodoItem(event) {
+    const todoItem = event.target.closest('.todo-item');
+    if (event.target.checked) {
+        todoItem.classList.add('todo-item-through');
+    } else {
+        todoItem.classList.remove('todo-item-through');
+    }
+}
+
 function addTask(){
     let tarefa = document.querySelector("input").value
 
     let li = document.createElement("li")
-    li.innerHTML = tarefa + '<i class="fa-solid fa-xmark todo-item-close" onclick="removeTask(this)">'
+    li.innerHTML = '<input type="checkbox" name="cb-task" onchange="toggleTodoItem(event)">' +  tarefa + '<i class="fa-solid fa-xmark todo-item-close" onclick="removeTask(this)">'
     li.classList.add("todo-item")
     document.querySelector("ul").appendChild(li)
     document.getElementById("task").value = ""
